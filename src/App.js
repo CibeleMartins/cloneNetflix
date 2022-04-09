b89357a4f819a3000691758a8514da5069c5e90a
Cibele <cibelemartins24@hotmail.com>
2022-04-08 23:01:15 -0300

    :package: create a function, test requests
    create a function to test requests in the console

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import Movie from './Movies';
 
 
 export default () => {
+
+  useEffect(()=>{
+
+    const loadAll = async () => {
+      // Pegando alista total de filmes
+      let list = await Movie.getHomeList();
+      console.log(list);
+    }
+    
+    loadAll()
+  }, [])
+  // essa função vai executar algo quando a tela carregar
 
   return (
     <h1>Ola mundo</h1>
